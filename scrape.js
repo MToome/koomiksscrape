9ba53334108aacf87b6d308e5d396800fd34ad9b
Marek Toome <marekjah@gmail.com>
2025-06-22 08:32:04 +0300

    Add sleep and for loo

--- scrape.js
+++ scrape.js
@@ -4,6 +4,9 @@ import * as cheerio from 'cheerio';
 // fs- filesystem
 import fs from 'fs';
 
+// promise koodi mis ootab millal miski on tehtud enne jätkamist, resolved kuni on täielikult lõpetanud
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 function getCache(name) {
     // Sync teeb asju samm sammult, ootab millal üks asi on lõpetanud ja siis teeb järgmise
     // Kui on siis tagastab andmed
@@ -14,10 +17,32 @@ function getCache(name) {
     }
 
 function setCache(name, value) {
-    if(!fs.existsSync(cache)){
-        fs.mkdirSync(cache)
+    if(!fs.existsSync('cache')){
+        fs.mkdirSync('cache')
     }
     fs.writeFileSync(`cache/${name}.html`, value, 'utf-8')
 }
     
+for(let i = 226; i<236; i++){
+    let data = getCache(`cache/${i}.html`)
+    if(!data){
+        sleep(2000);
+        const vastus = await fetch(`https://existentialcomics.com/comic/${i}`) 
+        data = await vastus.text();
+        setCache(i, data);
+        
+    }
+
+    // $ muutuja nimena märgitakse funktsiooni või objekti mis kasutatakse valimaks ja muutmaks HTML elementi
+    const $ = cheerio.load(data);
+    const comicTitle = $('div.title h3').text();
+    console.log(comicTitle);
+    const img =$('img.comicImg').each((i, el) => {
+        const src = $(el).attr('src');
+        const imgTile = $(el).attr('title') || 'no title on img' // kui undefined prindib no title on img;
+        console.log(imgTile, '\n', src) ;
+    });
+
+    console.log('\n');
+}
 
